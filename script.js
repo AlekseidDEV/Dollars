a1a615516dc373148d309b85a2c13228c606017d
AleksDev <lesha.rudiko@gmail.com>
2023-09-28 18:36:54 +0300

    dollar

--- script.js
+++ script.js
@@ -1,40 +1,41 @@
-// const selectTo = document.getElementById('selectTo')
-// const selectfrom = document.getElementById('selectFrom')
-// const inputSum = document.getElementById('inputSum')
-// const btn = document.getElementById('btn')
+const selectTo = document.getElementById("selectTo");
+const inputSum = document.getElementById("inputSum");
+const btn = document.getElementById("btn");
+const span = document.querySelector(".result");
 
-// const requestServ = async () => {
-//     const r = await fetch('https://www.cbr-xml-daily.ru/daily_json.js')
-//     return await r.json()
-// }
+const requestServ = async () => {
+    const r = await fetch("https://www.cbr-xml-daily.ru/daily_json.js");
+    return await r.json();
+};
 
-// const unpackingDate = (objVal) => {
-//     console.log(objVal);
-// }
+const calcValute = (dollar, euro, valTo, money) => {
+    let totalSum;
+    if (valTo === "USD") {
+        totalSum = Math.ceil(dollar.Value * money);
+    } else if (valTo === "EUR") {
+        totalSum = Math.ceil(euro.Value * money);
+    }
 
-// const getValue = () => {
-//     if(selectTo.value === '' || selectfrom.value === ''){
-//         alert('выберите валюту')
-//     } else if(inputSum.value === ''){
-//         alert('заполните поле "введите сумму"')
-//     } else{
-//         requestServ()
-//             .then((data) => unpackingDate(data.Valute))
-//             .catch(() => alert('Сервер не отвечает :('))
-//     }
-// }
+    span.textContent = `Итого : ${totalSum} RUB`;
+};
 
+const getValue = () => {
+    if (selectTo.value === "") {
+        alert("выберите валюту");
+    } else if (inputSum.value === "") {
+        alert('заполните поле "введите сумму"');
+    } else {
+        requestServ()
+            .then((data) =>
+                calcValute(
+                    data.Valute.USD,
+                    data.Valute.EUR,
+                    selectTo.value,
+                    +inputSum.value
+                )
+            )
+            .catch(() => alert("Сервер не отвечает :("));
+    }
+};
 
-// btn.addEventListener('click' , getValue)
-
-// const test = async () => {
-//     const r = await fetch('https://www.cbr-xml-daily.ru/daily_jsonp.js')
-//     return await r.json()
-// }
-
-// const test1 = (data) => {
-//     console.log(data);
-// }
-
-// test()
-//     .then((data) => test1(data))
+btn.addEventListener("click", getValue);
