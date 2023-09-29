96b7fd57b299b40300b10da769d8bf058c917d6f
AleksDev <lesha.rudiko@gmail.com>
2023-09-29 16:38:31 +0300

    finaly update

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const selectTo = document.getElementById("selectTo");
+const selectFrom = document.getElementById("selectFrom");
 const inputSum = document.getElementById("inputSum");
 const btn = document.getElementById("btn");
 const span = document.querySelector(".result");
@@ -8,15 +9,32 @@ const requestServ = async () => {
     return await r.json();
 };
 
-const calcValute = (dollar, euro, valTo, money) => {
+const calcValute = (dollar, euro, valTo, valFrom, money) => {
     let totalSum;
-    if (valTo === "USD") {
+
+    if (valTo === "USD" && valFrom === 'RUB') {
         totalSum = Math.ceil(dollar.Value * money);
-    } else if (valTo === "EUR") {
+        span.textContent = `Итого : ${totalSum} ${valFrom}`;
+    } else if (valTo === "EUR" && valFrom === 'RUB') {
         totalSum = Math.ceil(euro.Value * money);
+        span.textContent = `Итого : ${totalSum} ${valFrom}`;
+    }
+    
+    if(valTo === "RUB" && valFrom === 'USD'){
+        totalSum = money / dollar.Value
+        span.textContent = `Итого : ${totalSum.toFixed(4)} ${valFrom}`;
+    } else if(valTo === "RUB" && valFrom === 'EUR'){
+        totalSum = money / euro.Value
+        span.textContent = `Итого : ${totalSum.toFixed(4)} ${valFrom}`;
     }
 
-    span.textContent = `Итого : ${totalSum} RUB`;
+    if(valTo === "USD" && valFrom === 'EUR'){
+        totalSum = (euro.Value / 100) * money
+        span.textContent = `Итого : ${totalSum.toFixed(2)} ${valFrom}`;
+    } else if(valTo === "EUR" && valFrom === 'USD'){
+        totalSum = (dollar.Value / 100) * money
+        span.textContent = `Итого : ${totalSum.toFixed(2)} ${valFrom}`;
+    }
 };
 
 const getValue = () => {
@@ -24,6 +42,8 @@ const getValue = () => {
         alert("выберите валюту");
     } else if (inputSum.value === "") {
         alert('заполните поле "введите сумму"');
+    } else if(selectTo.value === selectFrom.value){
+        alert('нельзя перевести в ту же валюту')
     } else {
         requestServ()
             .then((data) =>
@@ -31,6 +51,7 @@ const getValue = () => {
                     data.Valute.USD,
                     data.Valute.EUR,
                     selectTo.value,
+                    selectFrom.value,
                     +inputSum.value
                 )
             )
